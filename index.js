abb17683c1a614e04289d2215ac2982e3e747ecc
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-04-06 10:38:29 -0400

    basic thing running

---
+++ index.js
@@ -0,0 +1,13 @@
+const   express = require('express'),
+        app = express(),
+        mustache = require('mustache-express'),
+        bodyParser = require('body-parser'),
+        fetch = require('fetch'),
+        PORT = process.env.PORT || 3000;
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, () => console.log('Server is listening on port', PORT));
