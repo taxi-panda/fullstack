128cb0f935eeb0552f44fcf6b1a248b2d82f72cd
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-04-06 15:33:12 -0400

    create function added

--- controllers/api/controller.js
+++ controllers/api/controller.js
@@ -9,7 +9,7 @@ controller.all = (req, res) => {
         .catch(err => console.log('Error: allRoutes:', err));
 };
 
-controller.create = (req,res) => {
+controller.createDummy = (req,res) => {
     const obj = {
         start_lat: faker.address.latitude(),
         start_lng: faker.address.longitude(),
@@ -22,6 +22,40 @@ controller.create = (req,res) => {
         price: faker.random.number(),
         start_time: "9:00"
     }
+
+    routes.create(obj)
+        .then(data => res.json(data))
+        .catch(err => console.log('Error: create:', err));
+}
+
+controller.create = (req, res) => {
+    // const obj = {
+    //     start_lat: req.body.start_lat,
+    //     start_lng: req.body.start_lng,
+    //     start_addr: req.body.start_addr,
+    //     start_name: req.body.start_name,
+    //     end_lat: req.body.end_lat,
+    //     end_lng: req.body.end_lng,
+    //     end_addr: req.body.end_addr,
+    //     end_name: req.body.end_name,
+    //     price: req.body.rpice,
+    //     start_time: req.body.start_time
+    // }
+
+    const obj = {};
+
+    const keys =
+    ["start_lat", "start_lng", "start_addr", "start_name","end_lat", "end_lng", "end_addr", "end_name", "price", "start_time"];
+
+    keys.forEach(key => {
+        if(req.body[key]){
+            obj[key] = req.body[key];
+        } else {
+            console.log("missing " + key);
+        }
+    });
+
+    console.log(obj);
 
     routes.create(obj)
         .then(data => res.json(data))
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,7 @@ const controller = require('./controller');
 
 router.get('/', controller.all);
 // ONLY POST HERE FOR DUMMY DATA!
+router.post('/dummy', controller.createDummy);
 router.post('/', controller.create);
 
 module.exports = router;
